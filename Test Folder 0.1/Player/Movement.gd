9ba8a75c858e5dab4dc8c2d1bba13d0c4896130c
Brian Anderson <briansoldado12@gmail.com>
2026-09-10 19:23:37 -0300

    Ajuste de aceleracion del personaje

--- Test Folder 0.1/Player/Movement.gd
+++ Test Folder 0.1/Player/Movement.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 
 # --- PARÁMETROS CONFIGURABLES ---
 @export var SPEED: float = 8.0
-@export var ACCEL: float = 12.0
+@export var ACCEL: float = 20.0
 @export var FRICTION: float = 30.0
 @export var JUMP_VELOCITY: float = 6.0
 @export var MAX_JUMPS: int = 2
